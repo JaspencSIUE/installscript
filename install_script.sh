4184e46d25e059fbb79df1b83522c0a766d77d6c
Jarod Spencer <jaspenc@node0.test2.siue-cs590-490.emulab.net>
2024-01-27 14:52:05 -0700

    all

---
+++ install_script.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+echo "updated"
+sudo apt-get install htop -y
+echo "installed htop"
+sudo apt-get install screen -y
+echo "installed screen"
+
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+echo "wget finished"
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+echo "bash line done"
+rm -rf ~/miniconda3/miniconda.sh
+echo "file removed"
