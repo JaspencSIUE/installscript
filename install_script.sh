dec1f5a50382dfafc3dfadc4e50244a979be13dd
JaspencSIUE <157921496+JaspencSIUE@users.noreply.github.com>
2024-02-13 10:50:00 -0600

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,14 +1,20 @@
-sudo apt-get update -y
-echo "updated"
-sudo apt-get install htop -y
-echo "installed htop"
-sudo apt-get install screen -y
-echo "installed screen"
+sudo apt-get update
+sudo apt install screen
+sudo apt install htop
 mkdir -p ~/miniconda3
-
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-echo "wget finished"
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-echo "bash line done"
 rm -rf ~/miniconda3/miniconda.sh
-echo "file removed"
+
+~/miniconda3/bin/conda init bash
+source ~/.bashrc
+
+conda create --name xyz python=3.9 -y
+conda active xyz
+
+conda install -c conda-forge notebook -y
+conda install -c conda-forge nb_conda_kernels -y
+conda install nb_conda -y
+
+screen -S myScreen
+conda activate xyz
