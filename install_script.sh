15c5c6d9d95065bf0dda31a2f5d1b58aa7932507
Jarod Spencer <jaspenc@node0.exptest1.siue-cs590-490.emulab.net>
2024-02-13 08:37:40 -0700

    newscript

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,7 @@ sudo apt-get install htop -y
 echo "installed htop"
 sudo apt-get install screen -y
 echo "installed screen"
-
+mkdir -p ~/miniconda3
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 echo "wget finished"
